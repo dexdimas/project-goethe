5b4196a45485e5c1d650057782f82fcf057df7a1
Andimas Muhammad Bagaswara <andimas.bagaswara@gmail.com>
2020-06-08 16:26:50 +0800

    initial commit

---
+++ project-goethe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project-goethe
+//
+//  Created by Andimas Bagaswara on 08/06/20.
+//  Copyright © 2020 Andimas Bagaswara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
